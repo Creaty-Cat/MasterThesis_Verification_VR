790a61d611daadb9b73af055c1a28d2531649cec
Creaty-Cat <lisadeschler2000@gmail.com>
2026-08-22 21:20:03 +0200

    Added rateButton functionality to Interface A

--- Assets/Scripts/CloseController.cs
+++ Assets/Scripts/CloseController.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class CloseController : MonoBehaviour
 {
     [SerializeField] private GameObject closeInterface;
+    [SerializeField] private GameObject openInterface;
 
     public void Close(GameObject closeInterface)
     {
         closeInterface.SetActive(false);
+    }
+
+    public void Open(GameObject openInterface)
+    {
+        openInterface.SetActive(true);
     }
 }
