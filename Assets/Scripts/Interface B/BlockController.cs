b76f68ccebd896f12620e40e798f11fe591c3d75
Creaty-Cat <lisadeschler2000@gmail.com>
2026-08-22 12:51:00 +0200

    Blocking Interface works.

--- Assets/Scripts/Interface B/BlockController.cs
+++ Assets/Scripts/Interface B/BlockController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class BlockController : MonoBehaviour
 {
     [SerializeField] private GameObject interfaceToClose;
+    [SerializeField] private GameObject blockInformation;
     [SerializeField] private GameObject avatar;
 
     public void BlockUser()
     {
+        blockInformation.SetActive(true);
         interfaceToClose.SetActive(false);
         avatar.SetActive(false);
+        
     }
 }
