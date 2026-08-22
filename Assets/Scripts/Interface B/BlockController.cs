e7d20f484630c1ab07e724481519eeecb9294f97
Creaty-Cat <lisadeschler2000@gmail.com>
2026-08-22 17:10:48 +0200

    Interface B has two working interfaces.

--- Assets/Scripts/Interface B/BlockController.cs
+++ Assets/Scripts/Interface B/BlockController.cs
@@ -5,12 +5,15 @@ public class BlockController : MonoBehaviour
     [SerializeField] private GameObject interfaceToClose;
     [SerializeField] private GameObject blockInformation;
     [SerializeField] private GameObject avatar;
+    [SerializeField] private GameObject badge;
+
 
     public void BlockUser()
     {
         blockInformation.SetActive(true);
         interfaceToClose.SetActive(false);
         avatar.SetActive(false);
+        badge.SetActive(false);
         
     }
 }
