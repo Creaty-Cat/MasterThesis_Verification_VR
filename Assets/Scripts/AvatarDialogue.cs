225df842779d340ec8fde49582af5085755d7fd8
Creaty-Cat <lisadeschler2000@gmail.com>
2026-08-24 13:38:02 +0200

    Dialogue popups work.

--- Assets/Scripts/AvatarDialogue.cs
+++ Assets/Scripts/AvatarDialogue.cs
@@ -7,12 +7,16 @@ public class AvatarDialogue : MonoBehaviour
 	public DialogueStep[] dialogue;
 
 	public GameObject dialogueBox;
-
 	public GameObject speechBubble;
+	public GameObject popup;
 
 	public TMP_Text avatarText;
+	public TMP_Text popupText;
 
 	public Button[] optionButtons;
+	public Button closePopup;
+
+
 
 	private int currentStep = 0;
 
@@ -22,29 +26,73 @@ public class AvatarDialogue : MonoBehaviour
 		speechBubble.SetActive(false);
 		//set dialogueBox visible
 		dialogueBox.SetActive(true);
+
+		//zur Sicherheit ausblenden
+		popup.SetActive(false);
+
 		currentStep = 0;
+
 		ShowDialogue();
 	}
 
-	void ShowDialogue(){
+	void ShowDialogue()
+	{
 		DialogueStep step = dialogue[currentStep];
 
 		avatarText.text = step.avatarText;
 
-		for (int i = 0; i < optionButtons.Length; i++){
-			if(i < step.userOptions.Length){
+		for (int i = 0; i < optionButtons.Length; i++)
+		{
+			if(i < step.userOptions.Length)
+			{
 				optionButtons[i].gameObject.SetActive(true);
 
-				optionButtons[i].GetComponentInChildren<TMP_Text>().text = step.userOptions[i];
+				optionButtons[i].GetComponentInChildren<TMP_Text>().text = step.userOptions[i].text;
 
 				optionButtons[i].onClick.RemoveAllListeners();
 
-				optionButtons[i].onClick.AddListener(NextDialogue);
+				int optionIndex = i;
+
+				optionButtons[i].onClick.AddListener(
+					() => SelectionOption(optionIndex)
+				);
 			}
-			else{
+			else
+			{
 				optionButtons[i].gameObject.SetActive(false);
 			}
 		}
+	}
+
+	void SelectionOption(int optionIndex)
+	{
+		DialogueOption selectedOption = dialogue[currentStep].userOptions[optionIndex];
+
+		if(selectedOption.openPopup)
+		{
+			ShowPopup(selectedOption.popupText);
+		}
+		else
+		{
+			NextDialogue();
+		}
+	}
+
+	void ShowPopup(string text)
+	{
+		popup.SetActive(true);
+
+		popupText.text = text;
+
+		closePopup.onClick.RemoveAllListeners();
+		closePopup.onClick.AddListener(ClosePopup);
+	}
+
+	void ClosePopup()
+	{
+		popup.SetActive(false);
+
+		NextDialogue();
 	}
 
 	void NextDialogue(){
--- Assets/Scripts/DialogueStep.cs
+++ Assets/Scripts/DialogueStep.cs
@@ -1,5 +1,18 @@
 using UnityEngine;
 
+[System.Serializable]
+public class DialogueOption
+{
+    public string text;
+
+    //soll Popup angezeigt werden?
+    public bool openPopup;
+
+    //Text der angezeigt wird
+    [TextArea(3,8)]
+    public string popupText;
+}
+
 
 [System.Serializable]
 public class DialogueStep
@@ -7,6 +20,6 @@ public class DialogueStep
     [TextArea(3, 8)]
     public string avatarText;
 
-    public string[] userOptions;
+    public DialogueOption[] userOptions;
     
 }
